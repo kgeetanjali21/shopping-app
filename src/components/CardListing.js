b7ac0be6e6fe9d10619de3f056d2412b8dd444ac
Geetanjali <geetakorgaonkar93@gmail.com>
2019-12-29 21:27:57 +0530

    box-shadow added to the cards

--- src/components/CardListing.js
+++ src/components/CardListing.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import insuranceData from '../insurance.json';
+import { content } from '../insurance.json';
 import InsuranceCard from './InsuranceCard.js';
 import '../assets/style/listing.scss';
 
 class CardListing extends Component {
     render() {
-        const { content } = insuranceData;
         console.log('content', content);
         return (
             <React.Fragment>
